c02c000e65e0ae4a7707ea281fc19b5d08f7e182
Natalya <m_d.r.lecter@protonmail.com>
2023-01-15 23:14:58 +0400

    start install

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/news', function () {
+    return view('welcome');
+});
+
+Route::get('/news/{name}', static function (string $name): string {
+    return "Hello, {$name}";
+});
+
+Route::get('/news/{info}', static function (): string {
+    return "Site with lots of news";
+});
+
+Route::get('/news/{id}', static function (int $id): string {
+    return "Piece of news num {$id}";
+});
